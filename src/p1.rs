51c9b96f28fb1274699007e53c1702da104b8f00
Chylli <chylli@binary.com>
2016-03-29 22:00:14 +0800

    change p1

--- src/p1.rs
+++ src/p1.rs
@@ -1,64 +1,11 @@
-///* Find the last box of a list.
-///** example
-///``` * (my-last '(a b c d))
-///(D)```
-use self::List::*;
+//!# Find the last box of a list.
+//!# example
+///```
+///use p99::p1::my_last;
+///assert_eq!(my_last(&['a','b','c','d']),['d'])
+///```
 
-enum List {
-    Cons(u32, Box<List>),
-    Nil,
+pub fn my_last(list: &[char]) -> [char; 1] {
+    [list[list.len()-1]]
 }
 
-impl List {
-    fn new()->List {
-        Nil
-    }
-
-    fn prepend(self, elem: u32) -> List {
-        Cons(elem, Box::new(self))
-    }
-
-    fn my_last(&self) -> List{
-        match *self {
-            // what's the meaning of ref here?
-            Cons(head, ref tail) => {
-                let tail_last = tail.my_last();
-                match tail_last {
-                    Nil => {
-                        Cons(head, Box::new(Nil))
-                    }
-                    _ => {tail_last}
-                }
-            }
-            Nil => {
-                Nil
-            }
-        }
-    }
-}
-
-#[test]
-fn my_last_test(){
-    let mut list = List::new();
-    list = list.prepend(1);
-    list = list.prepend(2);
-    list = list.prepend(3);
-    let last = list.my_last();
-    match last {
-        Nil => {
-            assert!(false);
-        }
-        // why cannot use ref here?
-        Cons(head, tail) => {
-            assert!(head == 1);
-            match *tail {
-                Nil => {
-                    assert!(true);
-                }
-                _ => {
-                    assert!(false);
-                }
-            }
-        }
-    }
-}
