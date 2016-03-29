8bdf0dec02de04a5f8c6ad0e51249cc0d8a66c00
Chylli <chylli@binary.com>
2016-03-29 22:25:46 +0800

    add more tests

--- src/p1.rs
+++ src/p1.rs
@@ -2,7 +2,9 @@
 //!# example
 ///```
 ///use p99::p1::my_last;
-///assert_eq!(my_last(&['a','b','c','d']),Some(['d']))
+///assert_eq!(my_last(&['a','b','c','d']),Some(['d']));
+///assert_eq!(my_last(&['d']),Some(['d']));
+///assert_eq!(my_last(&[]),None);
 ///```
 
 pub fn my_last(list: &[char]) -> Option<[char; 1]> {
