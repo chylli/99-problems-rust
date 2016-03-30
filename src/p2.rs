744a596398713a3ca2f1938021d82750c7f5f09f
Chylli <chylli@binary.com>
2016-03-30 21:07:31 +0800

    fix error

--- src/p2.rs
+++ src/p2.rs
@@ -2,14 +2,14 @@
 ///# example
 ///```
 ///use p99::p2::last_two;
-///assert_eq!(last_two(&['a','b','c','d']),Some(['c','d'])
-///assert_eq!(last_two(&['a']),None)
-///assert_eq!(last_two(&[]),None)
+///assert_eq!(last_two(&['a','b','c','d']),Some(['c','d']));
+///assert_eq!(last_two(&['a']),None);
+///assert_eq!(last_two(&[]),None);
 ///```
 
 pub fn last_two(list: &[char]) -> Option<[char; 2]> {
     if list.len() > 1{
-        Some([list[list.len()-1],list[list.len()]])
+        Some([list[list.len()-2],list[list.len()-1]])
     }
     else{
         None
