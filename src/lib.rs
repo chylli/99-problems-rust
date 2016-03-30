577314db52f0824abf0c65f69d36f239b879611c
Chylli <chylli@binary.com>
2016-03-30 20:47:19 +0800

    p2

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
 pub mod p1;
+//pub mod p2;
 
 
---
+++ src/p2.rs
@@ -0,0 +1,17 @@
+//!Find the last but one (last and penultimate) elements of a list. 
+///# example
+///```
+///use p99::p2::last_two;
+///assert_eq!(last_two(&['a','b','c','d']),Some(['c','d'])
+///assert_eq!(last_two(&['a']),None)
+///assert_eq!(last_two(&[]),None)
+///```
+
+pub fn last_two(list: &[char]) -> Option<[char; 2]> {
+    if list.len() > 1{
+        Some([list[list.len()-1],list[list.len()]])
+    }
+    else{
+        None
+    }
+}
