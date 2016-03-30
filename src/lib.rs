2f81f8a69eeb8da973d816a46a2e6d2c91b6ad2c
Chylli <chylli@binary.com>
2016-03-30 21:29:55 +0800

    p3 ok

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 pub mod p1;
-//pub mod p2;
+pub mod p2;
+pub mod p3;
 
 
---
+++ src/p3.rs
@@ -0,0 +1,15 @@
+//!Find the k'th element of a list. (easy)
+///# example
+///```
+///use p99::p3::at;
+///assert_eq!(at(3, &['a','b','c','d']), Some('c'));
+///assert_eq!(at(3, &['a','b',]), None)
+///```
+pub fn at(k: usize, list: &[char]) -> Option<char> {
+    if list.len() >= k {
+        Some(list[k-1])
+    }
+    else{
+        None
+    }
+}
