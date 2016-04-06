1667e6f41b6ac918a0c43ce5cffb32dce1e1890d
Chylli <chylli@binary.com>
2016-04-06 22:28:13 +0800

    p5 ok

---
+++ src/p5.rs
@@ -0,0 +1,18 @@
+//!Reverse a list. (easy)
+//!OCaml standard library has List.rev but we ask that you reimplement it.
+///# example
+///```
+///use p99::p5::rev;
+///assert_eq!(rev(&mut['a','b','c']),['c','b','a'])
+///```
+
+pub fn rev(list: &mut [char]) -> Vec<char> {
+    let len = list.len();
+    let index = len - 1;
+    let mut v = vec!['d';len];
+    for i in 0 .. len {
+        println!("{}", i);
+        v[index - i] = list[i];
+    }
+    v
+}
