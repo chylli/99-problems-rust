c36d8af5450d8d016c74f1a5af40056973f57fbb
Chylli <chylli@binary.com>
2016-04-07 20:57:56 +0800

    p8 ok

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,4 @@ pub mod p4;
 pub mod p5;
 pub mod p6;
 pub mod p7;
-
+pub mod p8;
---
+++ src/p8.rs
@@ -0,0 +1,20 @@
+//!Eliminate consecutive duplicates of list elements. (medium)
+//!# example
+//!```
+//!use p99::p8::compress;
+//!assert_eq!(compress(&['a','a','a','a','b','c','c','a','a','d','e','e','e','e']),['a','b','c','a','d','e']);
+//!
+//!```
+
+pub fn compress(list: &[char]) -> Vec<char>{
+    let mut result = vec![];
+    result.push(list[0]);
+    let mut last = list[0];
+    for c in list{
+        if *c != last {
+            last = *c;
+            result.push(last);
+        }
+    }
+    result
+}
