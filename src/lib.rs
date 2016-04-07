8984e17e592b15ee6ab19d072a199dfb973db8a9
Chylli <chylli@binary.com>
2016-04-07 19:12:17 +0800

    p6 ok

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ pub mod p2;
 pub mod p3;
 pub mod p4;
 pub mod p5;
+pub mod p6;
---
+++ src/p6.rs
@@ -0,0 +1,16 @@
+//!Find out whether a list is a palindrome. (easy)
+//!
+//!HINT: a palindrome is its own reverse.
+///# example
+///```
+/// use p99::p6::is_palindrome;
+///assert!(is_palindrome(&['a','b','a']));
+///assert!(!is_palindrome(&['a','b','c']));
+///```
+
+pub fn is_palindrome(list: &[char]) -> bool {
+    let mut new_list1 = list.to_vec();
+    let new_list2 = list.to_vec();
+    new_list1.reverse();
+    new_list1 == new_list2
+}
