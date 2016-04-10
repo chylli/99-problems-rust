452b7597041cbdc2e5aaafe9dee1bbb0338130b2
Chylli <chylli@binary.com>
2016-04-10 22:26:21 +0800

    v3

--- src/p14.rs
+++ src/p14.rs
@@ -9,6 +9,11 @@
 ///use p99::p14::duplicate2;
 ///assert_eq!(duplicate2(&['a','b','c']),vec!['a','a','b','b','c','c'])
 ///```
+///# Example
+///```
+///use p99::p14::duplicate3;
+///assert_eq!(duplicate3(&['a','b','c']),vec!['a','a','b','b','c','c'])
+///```
 
 
 pub fn duplicate(list: &[char]) -> Vec<char> {
@@ -28,4 +33,8 @@ pub fn duplicate2(list: &[char]) -> Vec<char> {
         sum
     });
     result
+}
+
+pub fn duplicate3(list: &[char]) -> Vec<char> {
+    list.iter().flat_map(|x| vec![*x,*x]).collect::<Vec<char>>()
 }
