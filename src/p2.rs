1d94e789aadb41f8b4034bc332afa71c340cfa5f
Chylli <chylli@binary.com>
2016-04-11 17:30:16 +0800

    generic p2

--- src/p2.rs
+++ src/p2.rs
@@ -4,12 +4,14 @@
 ///use p99::p2::last_two;
 ///assert_eq!(last_two(&['a','b','c','d']),Some(['c','d']));
 ///assert_eq!(last_two(&['a']),None);
-///assert_eq!(last_two(&[]),None);
+///let list:&[char] = &[];
+///assert_eq!(last_two(list),None);
 ///```
 
-pub fn last_two(list: &[char]) -> Option<[char; 2]> {
+use std::clone::Clone;
+pub fn last_two<T: PartialEq + Clone>(list: &[T]) -> Option<[T; 2]> {
     if list.len() > 1 {
-        Some([list[list.len() - 2], list[list.len() - 1]])
+        Some([list[list.len() - 2].clone(), list[list.len() - 1].clone()])
     } else {
         None
     }
