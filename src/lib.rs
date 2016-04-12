df9665aa4a9d5c0a4cb4570f12c4c1c13ad95bf6
Chylli <chylli@binary.com>
2016-04-12 21:44:29 +0800

    p18 ok

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,4 @@ pub mod p14;
 pub mod p15;
 pub mod p16;
 pub mod p17;
+pub mod p18;
---
+++ src/p18.rs
@@ -0,0 +1,26 @@
+//!Extract a slice from a list. (medium)
+//!
+//!Given two indices, i and k, the slice is the list containing the elements
+//!    between the i'th and k'th element of the original list (both limits
+//!                                                            included). Start
+//!    counting the elements with 0 (this is the way the List module numbers
+//!                                  elements).
+///```
+///use p99::p18::Slice;
+///let a: Vec<usize> = (0..100).collect();
+///assert_eq!(a.slice(10,20),(10..30).collect::<Vec<usize>>());
+///```
+
+pub trait Slice<T: Copy>{
+    fn slice(&self, start: usize, length: usize) -> Vec<T>;
+}
+
+impl <T: Copy> Slice<T> for Vec<T>{
+    fn slice(&self, start: usize, length: usize) -> Vec<T>{
+        let mut result = Vec::with_capacity(length);
+        for i in &self[start..start+length] {
+            result.push(*i);
+        }
+        result
+    }
+}
